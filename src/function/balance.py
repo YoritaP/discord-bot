f7b2c6937c2fbc0369aafd0160c78074e6730067
YoritaXP <m-e-l-o-n@outlook.jp>
2018-03-17 14:11:37 +0900

    [Fix] id is string

--- src/function/balance.py
+++ src/function/balance.py
@@ -4,13 +4,15 @@ import discord
 def getbalance(rpc, client, message):
     print("called : balance")
     
-    rpc.getaccountaddress(message.author.id)
-    address = rpc.getaddressesbyaccount(message.author.id)[0]
-    balance = float(rpc.getbalance(message.author.id))
+    id = str(message.author.id)
+
+    rpc.getaccountaddress(id)
+    address = rpc.getaddressesbyaccount(id)[0]
+    balance = float(rpc.getbalance(id))
     print(balance)
     responce = discord.Embed(title='Bitcoin balance', colour=0xDEADBF)
     
     responce.add_field(name="Address : ", value=address, inline=False)
-    responce.add_field(name="user : ", value="<@" + message.author.id + ">", inline=True)
+    responce.add_field(name="user : ", value="<@" + id + ">", inline=True)
     responce.add_field(name="btc : ", value=balance, inline=True)
     return responce
--- src/function/tip.py
+++ src/function/tip.py
@@ -9,15 +9,17 @@ def faucet(rpc, client, message):
     print("Amount : " + str(balance))
     amount = balance / 100
 
-    rpc.getaccountaddress(message.author.id)
-    address = rpc.getaddressesbyaccount(message.author.id)[0]
+    id = str(message.author.id)
+
+    rpc.getaccountaddress(id)
+    address = rpc.getaddressesbyaccount(id)[0]
 
     print(address)
     tx = rpc.sendtoaddress(address, amount)
 
     print(tx)
     responce = discord.Embed(title='Faucet', colour=0xDEADBF)
-    responce.add_field(name="user : ", value="<@" + message.author.id + ">", inline=False)
+    responce.add_field(name="user : ", value="<@" + id + ">", inline=False)
     responce.add_field(name="amount : ", value=amount, inline=False)
     responce.add_field(name="txid : ", value=tx, inline=False)
     return responce
